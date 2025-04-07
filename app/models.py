a561771cf2ee3b2be05a2fbc96e9730f37b26147
247731 Marszałek Piotr <247731@edu.p.lodz.pl>
2025-04-07 18:24:07 +0200

    Add properties to count tasks for project

--- app/models.py
+++ app/models.py
@@ -13,6 +13,20 @@ class Project(db.Model):
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     tasks = db.relationship('Task', backref='project', lazy='dynamic', cascade='all, delete-orphan')
 
+    @property
+    def task_count(self):
+        return self.tasks.count()
+
+    @property
+    def completed_task_count(self):
+        return self.tasks.filter_by(status='done').count()
+
+    @property
+    def progress_percentage(self):
+        if self.task_count == 0:
+            return 0
+        return int((self.completed_task_count / self.task_count) * 100)
+
 
 class Task(db.Model):
     __tablename__ = 'tasks'
